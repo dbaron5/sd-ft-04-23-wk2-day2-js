b19b1c5aac914921b6fef1eb654da2267c2f0102
dbaron5 <131029961+dbaron5@users.noreply.github.com>
2023-04-25 15:21:33 -0400

    end of day (see notes)

--- functions.js
+++ functions.js
@@ -17,3 +17,26 @@ function thisIsMyName(myName) {
 thisIsMyName("Daniel");
 
 // change your original function to accept a name parameter by adding name in between parenthesis on line 12
+
+// write a function that accepts 3 parameters
+// the parameters are as follows
+// height
+// weight
+// age
+
+function heightWeightAge(height, weight, age) {
+  // for height, write a message that says "Your Height is height"
+  // for weight, convert to kilos
+  // print a message that says "You weight approx this many kilos" (remove decimal/convert to whole number - hint: built it)
+  // for age, convert into seconds
+  console.log(height("My height is 5'11"));
+  console.log(weight.toKilos());
+  console.log(age);
+}
+
+function convertPoundsToKilograms(pounds) {
+  var kilograms = pounds * 0.453592;
+  return kilograms;
+}
+
+convertPoundsToKilograms(230);
