5cff1f45f776721b42b486266ec935e6d1878630
dbaron5 <131029961+dbaron5@users.noreply.github.com>
2023-04-25 11:19:12 -0400

    switch statements

--- conditionals.js
+++ conditionals.js
@@ -19,3 +19,23 @@ if (day === "Monday") {
 } else {
   console.log("No pets for you today");
 }
+
+const restaurant = "Chilis";
+
+switch (restaurant) {
+  case "Applebees":
+    console.log("Man that's too greasy");
+    break;
+  case "Ruby Tuesday":
+    console.log("Do these even exist anymore??");
+    break;
+  case "Red Lobster":
+    console.log("How old are those things in the tanks anyways?");
+    break;
+  case "Chilis":
+    console.log("Isn't this just more expensive Applebees?");
+    break;
+  default:
+    console.log("I guess McDonalds is gonna be on the menu");
+    break;
+}
